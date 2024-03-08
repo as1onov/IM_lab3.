56b8dc9af1bc16d1471dd626fa50ea7309da4d62
as1onov <144804399+as1onov@users.noreply.github.com>
2024-03-08 16:00:25 +0700

    commit
    ИМ_лаб3

---
+++ ИМ_лаба_3/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ИМ_лаба_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.button_stop);
+            this.splitContainer1.Panel1.Controls.Add(this.button_start);
+            this.splitContainer1.Panel1.Controls.Add(this.edRule);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1247, 564);
+            this.splitContainer1.SplitterDistance = 212;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button_stop
+            // 
+            this.button_stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_stop.Location = new System.Drawing.Point(107, 68);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(90, 40);
+            this.button_stop.TabIndex = 3;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = false;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_start.Location = new System.Drawing.Point(11, 68);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(90, 40);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // edRule
+            // 
+            this.edRule.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edRule.Location = new System.Drawing.Point(65, 25);
+            this.edRule.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(120, 27);
+            this.edRule.TabIndex = 1;
+            this.edRule.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.edRule.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(47, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rule";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1027, 560);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 564);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ИМ_лаба_3/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ИМ_лаба_3
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        int resolution = 5;
+        private bool[,] field;
+        private int rows;
+        private int cols;
+        int iRows;
+        int rule;
+        bool[] rules = new bool[8];
+        bool left, center, right;
+        int index;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        private void DrawField()
+        {
+            Pen borderPen = new Pen(Color.Black, 2);
+            graphics.DrawRectangle(borderPen, 0, 0, cols * resolution, rows * resolution);
+
+            // Рисуем сетку игрового поля
+            for (int i = 1; i < rows; i++)
+            {
+                graphics.DrawLine(Pens.Black, 0, i * resolution, cols * resolution, i * resolution);
+            }
+
+            for (int j = 1; j < cols; j++)
+            {
+                graphics.DrawLine(Pens.Black, j * resolution, 0, j * resolution, rows * resolution);
+            }
+
+            
+        }
+
+        private void FirstGeneration() {
+            for (int i = 0; i < cols; ++i)
+            {
+                if (field[i, 0] == true)
+                {
+                    graphics.FillRectangle(Brushes.Red, i * resolution + 1, 0 * resolution + 1, resolution - 1, resolution-1);
+
+                }
+            }
+
+        }
+        private void NextGeneration(int j, int iRows)
+        {
+            for (int i = 0; i < cols; ++i)
+            {
+                if ((field[i, iRows] == true) && (iRows < rows))
+                {
+                    graphics.FillRectangle(Brushes.Red, i * resolution + 1, iRows * resolution + 1, resolution - 1, resolution - 1);
+
+                }
+            }
+
+        }
+        private void StartGame()
+        {
+        if (timer1.Enabled == true) return;
+
+            rule = (int)edRule.Value; 
+            cols = pictureBox1.Width / resolution; 
+            rows = pictureBox1.Height / resolution;
+            
+            iRows = 1;
+
+            field = new bool[cols, rows];
+            Random random = new Random();
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    field[i, j] = false;
+                }
+            }
+
+            for (int i = 1; i < cols; i++) 
+            {
+                 field[i, 0] = random.Next(2) == 0;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                rules[i] = (rule & (1 << i)) != 0;
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+
+            // Рисуем сетку игрового поля
+            DrawField();
+            FirstGeneration();
+
+
+            timer1.Start();
+
+        }
+   
+        private void Myrules()
+        {
+            if(left && center && right) { index  = 0; }
+            else if (left && center && !right) { index = 1; }
+            else if (left && !center && right) { index = 2; }
+            else if (left && !center && !right) { index = 3; }
+            else if (!left && center && right) { index = 4; }
+            else if (!left && center && !right) { index = 5; }
+            else if (!left && !center && right) { index = 6; }
+            else if (!left && !center && !right) { index = 7; }
+        }
+        
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            for (int j = 0; j < cols; j++)
+            {
+                if (j == 0 && iRows < rows)
+                {
+                    left = field[cols - 1, iRows-1];
+                    center = field[j, iRows-1];
+                    right = field[j + 1, iRows - 1];
+                }
+                else if (j == cols - 1)
+                {
+                    left = field[j - 1, iRows - 1];
+                    center = field[j, iRows - 1];
+                    right = field[0, iRows - 1];
+                }
+                else if (iRows < rows)
+                {
+                    left = field[j - 1, iRows - 1];
+                    center = field[j, iRows - 1];
+                    right = field[j + 1, iRows - 1];
+                }
+
+                Myrules();
+                field[j, iRows] = rules[index];
+
+                NextGeneration(j, iRows);
+                pictureBox1.Invalidate();
+                
+
+            }
+            if (iRows < rows - 1) iRows += 1;
+            else timer1.Stop();
+
+
+            
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            
+            
+        }
+    }
+}
